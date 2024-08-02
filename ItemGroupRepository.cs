f2bba5f08a1dfb2716bd3972e25db51f76dd0353
afterDark9091 <afterDark9091@protonmail.com>
2024-08-02 15:27:50 +0300

    energyStorage

---
+++ ItemGroupRepository.cs
@@ -0,0 +1,44 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Extensions;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(IItemGroupRepository))]
+    public class ItemGroupRepository : BaseRepository<ItemGroupEntity, int>, IItemGroupRepository
+    {
+        public async Task<ListQueryResult<ItemGroupView>> GetItemGroupsByApplicationRef(QueryDataSource<ItemGroupView> searchQuery, int languageRef)
+        {
+            var result = new ListQueryResult<ItemGroupView>
+            {
+                Entities = new List<ItemGroupView>()
+            };
+
+            var queryResult = from ig in GenericRepository.Query<ItemGroupEntity>()
+                              join igl in GenericRepository.Query<ItemGroupLanguageEntity>()
+                              on ig.Key equals igl.ItemGroupRef
+                              where igl.LanguageRef == languageRef
+                              select new ItemGroupView
+                              {
+                                  Key = ig.Key,
+                                  ApplicationRef = ig.ApplicationRef,
+                                  Alias = ig.Alias,
+                                  IsActive = ig.IsActive,
+                                  _Title = igl._Title,
+                                  ItemGroupRef = igl.ItemGroupRef
+                              };
+
+            result = await queryResult.ToListQueryResultAsync(searchQuery);
+
+            return result;
+        }
+    }
+}
---
+++ ItemRowLanguageRepository.cs
@@ -0,0 +1,35 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using ICD.Framework.Extensions;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(IItemRowLanguageRepository))]
+    public class ItemRowLanguageRepository : BaseRepository<ItemRowLanguageEntity, int>, IItemRowLanguageRepository
+    {
+        public async Task<ListQueryResult<ItemRowLanguageEntity>> GetItemRowLanguagesAsync(QueryDataSource<ItemRowLanguageEntity> queryDataSource, int languageRef)
+        {
+            var result = new ListQueryResult<ItemRowLanguageEntity>
+            {
+                Entities = new List<ItemRowLanguageEntity>()
+            };
+
+            var queryResult = from irl in GenericRepository.Query<ItemRowLanguageEntity>()
+                              where irl.LanguageRef == languageRef
+                              select irl;
+
+            result = await queryResult.ToListQueryResultAsync(queryDataSource);
+
+            return result;
+        }
+    }
+}
